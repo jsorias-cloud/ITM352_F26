a7a9b8910944a6d0c8ab11163df6edeba9d42070
jsorias-cloud <jsorias@hawaii.edu>
2026-09-02 12:57:49 -1000

    Exercise 2

---
+++ newdir/Lab2/Ex1.py
@@ -0,0 +1,15 @@
+#This program prompts the user to enter a number between 1 and 100, then calculates and 
+# displays the square of that number and then prints both the entered number and its square
+# Name: Jorryn Orias
+#Date: Sept 2
+
+value_entered = input("Enter a number between 1 and 100: ")
+value = int(value_entered)
+
+valueSquared = value_as_integer_ ** 2
+
+print ("You entered:", value_as_integer)
+print("The square of the number is:", valueSquared)
+
+print(f"You entered: (value_as_integer), and the square of the numbr is: (valueSquared)")
+
