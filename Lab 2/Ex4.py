e57e2da5a87c58a508e3c2d252fbd4a13f8a4c95
jsorias-cloud <jsorias@hawaii.edu>
2026-09-02 13:13:35 -1000

    Exercises 3&4

---
+++ Lab 2/Ex4.py
@@ -0,0 +1,11 @@
+# Ask the user to enter a decimal number. Calculate its square of that number and print it out.
+# Name: Jorryn Orias
+# Date: Sept 2, 2026
+
+input_value = input("Enter a floating point number: ")
+float_value = float(input_value)
+squared_value = float_value ** 2
+rounded_value = round(squared_value, 2)
+
+print("You entered:", float_value)
+print("The square of the number you entered is:", rounded_value) 
