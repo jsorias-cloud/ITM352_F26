83986f504d5d72a8cdd0431671322072e58847b4
jsorias-cloud <jsorias@hawaii.edu>
2026-09-16 23:30:48 -1000

    Update Ex1.py

--- Lab 4/Ex1.py
+++ Lab 4/Ex1.py
@@ -2,7 +2,7 @@ first=input("Enter your first name: ")
 middle_initial=input("Enter your middle initial: ")
 last=input("Enter your last name: ")
 
-full_name=first+ " "+ middle_initial + " "+last
+full_name=first+ " "+ middle_initial + "."+last
 print("Your full name is:", full_name)
 
 print(f"Your full name using f-strings is: {first} {middle_initial}. {last}")
